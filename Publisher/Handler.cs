5cc7e73c3427eb59809e19fe4154a6fadb90540e
BrunoSFreschi <bruno.freschi2@gmail.com>
2025-03-04 23:13:00 -0300

    generic Handler

---
+++ Publisher/Handler.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher;
+
+class Handler
+{
+    internal static async Task Delivery(string channelName, string message)
+    {
+        ConnectionFactory factory = new() { HostName = "localhost" };
+
+        try
+        {
+            using IConnection connection = await factory.CreateConnectionAsync();
+
+            using IChannel channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: channelName,
+                                           durable: false,
+                                           exclusive: false,
+                                           autoDelete: false,
+                                           arguments: null);
+
+
+            byte[] body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(exchange: "",
+                                            routingKey: channelName,
+                                            body: body);
+
+            Console.WriteLine($" [x] Sent {message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao conectar ao RabbitMQ: {ex.Message}");
+            Console.WriteLine($"Detalhes: {ex.InnerException?.Message}");
+        }
+    }
+}
