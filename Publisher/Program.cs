b56a806f265dae8b680c8bd23566122a740e05c0
BrunoSFreschi <bruno.freschi2@gmail.com>
2025-03-04 23:12:44 -0300

    create

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Publisher;
+
+
+string channel = "Hello";
+
+string message = "Hello RabbitMQ!";
+
+
+await Handler.Delivery(channel, message);
+
+
+Console.WriteLine(" Press [enter] to exit.");
+
